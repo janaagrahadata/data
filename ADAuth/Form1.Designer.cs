26f318a754ac497de002b1de860b2cfd72aa9c88
Babu Thomas <babu.thomas@JUF.local>
2018-10-12 10:22:26 +0530

    First Commit

---
+++ ADAuth/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ADAuth
+{
+    partial class frmADAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.edtUserName = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.edtPassword = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(273, 141);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(142, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check AD Authentication";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(35, 41);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(60, 13);
+            this.lblUserName.TabIndex = 1;
+            this.lblUserName.Text = "User Name";
+            // 
+            // edtUserName
+            // 
+            this.edtUserName.Location = new System.Drawing.Point(136, 41);
+            this.edtUserName.Name = "edtUserName";
+            this.edtUserName.Size = new System.Drawing.Size(279, 20);
+            this.edtUserName.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(35, 80);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // edtPassword
+            // 
+            this.edtPassword.Location = new System.Drawing.Point(136, 73);
+            this.edtPassword.Name = "edtPassword";
+            this.edtPassword.PasswordChar = '*';
+            this.edtPassword.Size = new System.Drawing.Size(279, 20);
+            this.edtPassword.TabIndex = 4;
+            this.edtPassword.WordWrap = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(35, 196);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 5;
+            // 
+            // frmADAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 270);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.edtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.edtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "frmADAuth";
+            this.Text = "Check AD Authentication";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox edtUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox edtPassword;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ ADAuth/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.Protocols;
+using System.Net;
+
+
+namespace ADAuth
+{
+    public partial class frmADAuth : Form
+    {
+        public frmADAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LdapConnection connection = new LdapConnection("juf.local");
+                NetworkCredential credential = new NetworkCredential(edtUserName.Text, edtPassword.Text);
+                connection.Credential = credential;
+                connection.Bind();
+                lblResult.Text = "Logged In";
+            }
+            catch (LdapException lexc)
+            {
+                string error = lexc.ServerErrorMessage;
+                lblResult.Text = lexc.ServerErrorMessage;
+            }
+            catch (Exception exc)
+            {
+                lblResult.Text = exc.Message;
+            }
+        }
+    }
+}

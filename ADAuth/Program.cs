95b24deab8fb5e33ef81048c3a0ba5e27e08d0b8
Babu <data@janaagraha.org>
2018-10-15 16:02:34 +0530

    Add More Description section

--- ADAuth/Program.cs
+++ ADAuth/Program.cs
@@ -11,7 +11,6 @@ namespace ADAuth
         /// <summary>
         /// The main entry point for the application.
 		/// More description to be added here
-		/// More description to be added here
         /// </summary>
         [STAThread]
         static void Main()
